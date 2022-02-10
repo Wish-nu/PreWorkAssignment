776638b0e505911eb628365bc66edfad2fbd785c
Wish-nu <81408708+Wish-nu@users.noreply.github.com>
2022-02-10 11:08:19 +0530

    Palindrome
    using for & if/else statement

--- Main.java
+++ Main.java
@@ -4,54 +4,37 @@ package com.assignment;
 import java.util.Scanner;
 
 public class Main {
-    Scanner sc = new Scanner(System.in);
-
-    public void checkPalindrome(){
-    }
-
-    public static void main (String[] args) {
-        Main obj = new Main();
-
-        int choice;
-
+    public static void main(String args[]){
+        int checkNum;
+        int x;
+        int y;
+        int r;
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to check for palindrome: ");
+        checkNum = sc.nextInt();
+        x = checkNum;
+        for (y=0; checkNum>0; checkNum/=10)
+        {
+            r = checkNum%10;
+            y = (y*10)+r;
 
-        do {
-            System.out.println("Enter your choice from below list.\n" + "Find Palindrome of number.\n");
-            System.out.println();
-
-            choice = sc.nextInt();
 
         }
-        switch (choice) {
+        if (y==x)
+            System.out.println(x + " is a Palindrome number");
+        else
+            System.out.println(x + " is not a Palindrome number");
 
-            case 0
-                ;
-                choice = 0;
-                break;
-
-
-            case 1: {
-                obj.checkPalindrome();
-            }
-            break;
-
-            default:
-                System.out.println("Invalid choice");
-
-        }
-        while (choice != 0) {
-            System.out.println("Excited successfully!!");
-            sc.close();
-
-        }
 
 
 
 
 
     }
+
 }
+
+
 
 
 
